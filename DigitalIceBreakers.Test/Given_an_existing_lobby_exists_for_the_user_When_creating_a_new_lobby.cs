cbce4d312b5c98a36fd1bd6a2d1dca7e1895eb85
stafford <stafford.williams@gmail.com>
2018-12-06 20:05:43 +1100

    create lobby test

--- DigitalIceBreakers.Test/Given_an_existing_lobby_exists_for_the_user_When_creating_a_new_lobby.cs
+++ DigitalIceBreakers.Test/Given_an_existing_lobby_exists_for_the_user_When_creating_a_new_lobby.cs
@@ -21,19 +21,8 @@ namespace DigitalIceBreakers.Test
             var playerId = Guid.NewGuid();
             _lobbys = new List<Lobby> { new Lobby { Id = Guid.NewGuid(), Players = new List<Player> { new Player { Id = playerId, IsAdmin = true } } } };
 
-            var gameHub = GetMockGameHub(playerId, _lobbys);
+            var gameHub = ObjectMother.GetMockGameHub(playerId, _lobbys);
             gameHub.CreateLobby(Guid.NewGuid(), null, new User(playerId, "")).Wait();
-        }
-
-        static GameHub GetMockGameHub(Guid playerId, List<Lobby> lobbys)
-        {
-            var gameHub = new GameHub(new Mock<ILogger<GameHub>>().Object, lobbys);
-            gameHub.Context = new Mock<HubCallerContext>().Object;
-            var clients = new Mock<IHubCallerClients>();
-            clients.Setup(p => p.Client(It.IsAny<string>())).Returns(new Mock<IClientProxy>().Object);
-            clients.SetupGet(p => p.Caller).Returns(new Mock<IClientProxy>().Object);
-            gameHub.Clients = clients.Object;
-            return gameHub;
         }
 
         [TestMethod]
