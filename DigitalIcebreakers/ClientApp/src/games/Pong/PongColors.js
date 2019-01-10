fce4826d4e67fdea5ba9f3467e02f0a3d842b727
stafford <stafford.williams@gmail.com>
2019-01-10 20:32:01 +1100

    paddles & ball

---
+++ DigitalIcebreakers/ClientApp/src/games/Pong/PongColors.js
@@ -0,0 +1,12 @@
+import {Colors} from '../../Colors';
+
+export const PongColors = {
+    LeftPaddleUp: Colors.Blue.C600,
+    LeftPaddleDown: Colors.Blue.C900,
+    LeftPaddleOver: Colors.Blue.C400,
+    RightPaddleUp: Colors.Red.C600,
+    RightPaddleDown: Colors.Red.C900,
+    RightPaddleOver: Colors.Red.C400,
+    Background: Colors.BlueGrey.C800,
+    Ball: Colors.Teal.C500
+};
--- DigitalIcebreakers/ClientApp/src/games/Pong/PongPresenter.js
+++ DigitalIcebreakers/ClientApp/src/games/Pong/PongPresenter.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Form, FormGroup, ControlLabel, FormControl } from "react-bootstrap";
+import { PongColors as Colors } from './PongColors';
 import * as PIXI from "pixi.js";
+
+const defaultSpeed = 10;
+const defaultHeight = 7;
+const defaultWidth = 30;
 
 export class PongPresenter extends Component {
     displayName = PongPresenter.name
@@ -8,16 +13,20 @@ export class PongPresenter extends Component {
     constructor(props, context) {
         super(props, context);
 
-        this.app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight, backgroundColor: 0xFF0000 });
+        this.app = new PIXI.Application({ autoResize: true, backgroundColor: Colors.Background });
 
         this.pixiElement = null;
 
         this.state = {
-            players: []
+            left: 0,
+            right: 0,
+            paddleWidth: defaultWidth,
+            paddleHeight: defaultHeight,
+            speed: defaultSpeed
         };
 
         this.props.connection.on("gameUpdate", (response) => {
-            console.log(response);
+            this.setState(response);
         });
     }
 
@@ -26,23 +35,76 @@ export class PongPresenter extends Component {
 
         if (this.pixiElement && this.pixiElement.children.length <= 0) {
             this.pixiElement.appendChild(this.app.view);
+            this.app.renderer.resize(element.clientWidth, element.clientHeight);
+
+            this.init(element);    
         }
     }
 
-    render() {
-        const players = (this.state.players || []).map((p, ix) => {
-            if (p.state === "down")
-                return <ListGroupItem key={ix} active>{p.name}</ListGroupItem>;
-            else
-                return <ListGroupItem key={ix}>{p.name}</ListGroupItem>;
-        });
+    init() {
+        const element = this.pixiElement;
+        const paddleWidth = element.clientWidth/(this.state.paddleWidth || defaultWidth);
+        const paddleHeight = element.clientHeight/(this.state.paddleHeight || defaultHeight);
 
+        if (this.leftPaddle)
+            this.app.stage.removeChild(this.leftPaddle);
+
+        if (this.rightPaddle)
+            this.app.stage.removeChild(this.rightPaddle);
+
+        if (this.ball)
+            this.app.stage.removeChild(this.ball);
+
+        this.leftPaddle = this.getBlock(Colors.LeftPaddleUp, paddleWidth, paddleHeight);
+        this.rightPaddle = this.getBlock(Colors.RightPaddleUp, paddleWidth, paddleHeight);
+        this.ball = this.getBlock(Colors.Ball, paddleWidth, paddleWidth);
+
+        this.leftPaddle.position.set(paddleWidth, element.clientHeight/2);
+        this.rightPaddle.position.set(element.clientWidth - paddleWidth, element.clientHeight/2);
+        this.ball.position.set(element.clientWidth/2, element.clientHeight/2);
+
+        this.app.stage.addChild(this.leftPaddle, this.rightPaddle, this.ball);
+    }
+
+    getBlock(color, width, height) {
+        const g = new PIXI.Graphics();
+        g.beginFill(color);
+        g.drawRect(0, 0, width, height);
+        g.pivot.set(width/2, height/2);
+        return g;
+    }
+
+    updateHeight = (e) => {
+        this.setState({paddleHeight: e.target.value}, this.init);
+    }
+
+    updateWidth = (e) => {
+        this.setState({paddleWidth: e.target.value}, this.init);
+    }
+
+    updateSpeed = (e) => {
+        this.setState({speed: e.target.value}, this.init);
+    }
+ 
+    render() {
         return (
             <div>
                 <h2>Pong</h2>
-                <ListGroup>
-                    {players}
-                </ListGroup>
+                <div ref={this.pixiUpdate} />
+                <Form inline>
+                    <FormGroup controlId="formInlineName">
+                        <ControlLabel>Width</ControlLabel>{' '}
+                        <FormControl type="text" placeholder="Paddle width" value={this.state.paddleWidth} onChange={this.updateWidth} />
+                    </FormGroup>{' '}
+                    <FormGroup controlId="formInlineName">
+                        <ControlLabel>Height</ControlLabel>{' '}
+                        <FormControl type="text" placeholder="Paddle height" value={this.state.paddleHeight} onChange={this.updateHeight} />
+                    </FormGroup>{' '}
+                    <FormGroup controlId="formInlineName">
+                        <ControlLabel>Speed</ControlLabel>{' '}
+                        <FormControl type="text" placeholder="Speed" value={this.state.speed} onChange={this.updateSpeed} />
+                    </FormGroup>{' '}
+                    </Form>;
             </div>
         );
     }
