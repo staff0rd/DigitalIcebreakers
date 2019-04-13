b3637ed845088e08bffcac16907136956f315a2f
stafford <stafford.williams@gmail.com>
2019-04-13 18:51:04 +1000

    deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,20 @@
+sudo apt update
+sudo apt install docker.io -y 
+sudo apt install docker-compose -y
+
+# install microsoft signing key
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor | \
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+# add azure cli software repo
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+# install azure cli
+sudo apt-get update
+sudo apt-get install azure-cli
+
+az login --identity
+sudo az acr login --name stafford
+sudo docker run -p "80:80" --restart always stafford.azurecr.io/digitalicebreakers
---
+++ deploy/encode_script.sh
@@ -0,0 +1,1 @@
+cat deploy.sh | gzip -9 | base64 -w 0
