31660cd357ecf7682e5309c8372c600d6bb076b9
stafford <stafford.williams@gmail.com>
2019-04-14 01:51:10 +1000

    compose restart

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -17,7 +17,6 @@ sudo apt-get install azure-cli
 mkdir /vsts-agent;cd /vsts-agent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.149.2/vsts-agent-linux-x64-2.149.2.tar.gz;tar -zxvf vstsagent.tar.gz; 
 ./config.sh --deploymentgroup --deploymentgroupname "default" --addDeploymentGroupTags --replace --deploymentGroupTags signalr --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/staffordwilliams/ --work _work --projectname 'digitalicebreakers' --auth PAT --token tb7bogiv3yww42umfdoh55iitrj2kxqhanwigp4gw4qqszr25owa --runasservice; sudo ./svc.sh install; sudo ./svc.sh start; 
 
-
 # launch the contanier
 az login --identity
 sudo az acr login --name stafford
