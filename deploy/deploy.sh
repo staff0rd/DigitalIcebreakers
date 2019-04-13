d8bafc8ecd8255692eb4e2eb333998ac9055cbdf
stafford <stafford.williams@gmail.com>
2019-04-14 01:10:37 +1000

    install agent

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,6 +13,12 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 sudo apt-get update
 sudo apt-get install azure-cli
 
+# install vsts-agent
+mkdir /vsts-agent;cd /vsts-agent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.149.2/vsts-agent-linux-x64-2.149.2.tar.gz;tar -zxvf vstsagent.tar.gz; 
+./config.sh --deploymentgroup --deploymentgroupname "default" --addDeploymentGroupTags --replace --deploymentGroupTags signalr --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/staffordwilliams/ --work _work --projectname 'digitalicebreakers' --auth PAT --token tb7bogiv3yww42umfdoh55iitrj2kxqhanwigp4gw4qqszr25owa --runasservice; sudo ./svc.sh install; sudo ./svc.sh start; 
+
+
+# launch the contanier
 az login --identity
 sudo az acr login --name stafford
 sudo docker stop digital-icebreakers || true && sudo docker rm digital-icebreakers || true
