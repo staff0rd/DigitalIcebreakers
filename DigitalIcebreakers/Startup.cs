b4dd6acb2772cbb379721d46e5844d4d3f480224
stafford <stafford.williams@gmail.com>
2019-04-14 22:57:14 +1000

    application insights

--- DigitalIcebreakers/Startup.cs
+++ DigitalIcebreakers/Startup.cs
@@ -22,6 +22,8 @@ namespace DigitalIcebreakers
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSignalR();
