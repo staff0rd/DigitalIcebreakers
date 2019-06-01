67c3839c718b46208bf38dd5b7ce3b86681eec2e
stafford <stafford.williams@gmail.com>
2019-06-01 20:20:08 +1000

    typescript

---
+++ DigitalIcebreakers/ClientApp/src/Events.ts
@@ -0,0 +1,20 @@
+interface Event {
+    name: string;
+    label: string;
+    handler: Function;
+}
+
+export const Events = {
+    handlers: [] as Event[],
+    handle: (name: string) => {
+        Events.handlers
+            .filter((e) => e.name === name)
+            .forEach((e) => { e.handler(); });
+    },
+    add: (name: string, label: string, handler: Function) => {
+        Events.handlers.push({name: name, label: label, handler: handler});
+    },
+    remove: (name: string, label: string) => {
+        Events.handlers = Events.handlers.filter((e) => !(e.label === label && e.name === name));
+    }
+}
--- DigitalIcebreakers/ClientApp/src/Random.ts
+++ DigitalIcebreakers/ClientApp/src/Random.ts
@@ -1,7 +1,7 @@
-export function between(min, max) {
+export function between(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function pick(array) {
+export function pick(array: number[]) {
     return array[between(0, array.length - 1)];
 }
