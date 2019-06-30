7daba04e51f02374f74563549efef8cfb4916df7
Stafford Williams <stafford.williams@gmail.com>
2019-06-30 11:57:01 +1000

    test fix

--- DigitalIceBreakers.Test/Pong/Given_unequal_teams_When_a_player_joins.cs
+++ DigitalIceBreakers.Test/Pong/Given_unequal_teams_When_a_player_joins.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DigitalIceBreakers.Test.Pong
 {
@@ -14,13 +16,17 @@ namespace DigitalIceBreakers.Test.Pong
         Dictionary<Guid, int> _leftTeam = new Dictionary<Guid, int> { { Guid.NewGuid(), 0 } };
         Dictionary<Guid, int> _rightTeam = new Dictionary<Guid, int>();
 
-        public Given_unequal_teams_When_a_player_joins()
+        [TestInitialize]
+        public async Task Setup()
         {
             var game = new DigitalIcebreakers.Games.Pong(_leftTeam, _rightTeam);
             var lobby = ObjectMother.GetLobby(Guid.NewGuid(), game);
             lobby.Players.Add(ObjectMother.GetPlayer(playerId));
             var gameHub = ObjectMother.GetMockGameHub(playerId, lobby);
-            game.Message("join", gameHub).Wait();
+            var payload = JsonConvert.SerializeObject(new {
+                client = "join"
+            });
+            await gameHub.HubMessage(payload);
         }
 
         [TestMethod]
