e628f02d8a6fbe5ffacb08a997140c6b0be5f65b
Stafford Williams <stafford.williams@gmail.com>
2019-07-02 19:05:58 +1000

    test clean

--- DigitalIceBreakers.Test/StartStopContinue/IdeaSerialization.cs
+++ DigitalIceBreakers.Test/StartStopContinue/IdeaSerialization.cs
@@ -1,9 +1,6 @@
-using DigitalIcebreakers.Games;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Shouldly;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DigitalIceBreakers.Test
@@ -11,11 +8,10 @@ namespace DigitalIceBreakers.Test
     [TestClass]
     public class IdeaSerialization
     {
-        private MockGamehub _gameHub;
         private Idea _idea;
 
         [TestInitialize]
-        public async Task Setup()
+        public void Setup()
         {
             string payload = JsonConvert.SerializeObject(new {
                 client = new {content = "CONTENT", lane = 0}
