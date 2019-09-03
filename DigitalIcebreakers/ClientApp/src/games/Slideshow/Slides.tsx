8abff71ff6a755e0c46b952e29db4d4e2dcfc6c8
Stafford Williams <stafford.williams@gmail.com>
2019-09-03 19:19:05 +1000

    pushing the limit

--- DigitalIcebreakers/ClientApp/src/games/Slideshow/Slides.tsx
+++ DigitalIcebreakers/ClientApp/src/games/Slideshow/Slides.tsx
@@ -130,7 +130,7 @@ export class Slides extends Component<{}, SlidesState> {
 					`}</code></pre>
 				</section>
 				<section>
-					<h1>Pushing<br/>the limit</h1>
+					<h1>Pushing the limit</h1>
 				</section>
 				<section data-background-color="#ffffff">
 					<h2>Crankier</h2>
@@ -138,12 +138,62 @@ export class Slides extends Component<{}, SlidesState> {
 				<section data-background-color="#ffffff">
 					<table>
 						<tr>
-							<td>Local</td><td>&gt;&gt;</td><td>S1 App<br/>Service</td>
+							<td style={{verticalAlign:"middle"}}>Local</td><td style={{verticalAlign:"middle"}}>&gt;&gt;</td><td>S1 App<br/>Service</td>
 						</tr>
 						<tr>
 							<td colSpan={3} style={{textAlign:'center'}}>768 connections</td>
 						</tr>
 					</table>
+				</section>
+				<section data-background-color="#ffffff">
+					<table>
+						<tr>
+							<td style={{verticalAlign:"middle", textAlign:"center"}}>30-50 <br/>Containers</td><td style={{verticalAlign:"middle"}}>&gt;&gt;</td><td>S1 App<br/>Service</td>
+						</tr>
+						<tr>
+							<td colSpan={3} style={{textAlign:'center'}}>16,000 connections</td>
+						</tr>
+					</table>
+				</section>
+				<section data-background-color="#ffffff">
+					<table>
+						<tr>
+							<td style={{verticalAlign:"middle", textAlign:"center"}}>30-50 <br/>Containers</td><td style={{verticalAlign:"middle"}}>&gt;&gt;</td><td>D2s<br/>VM</td>
+						</tr>
+						<tr>
+							<td colSpan={3} style={{textAlign:'center'}}>65,000 connections</td>
+						</tr>
+					</table>
+				</section>
+				<section data-background-color="#ffffff">
+					<table>
+						<tr>
+							<td style={{verticalAlign:"middle", textAlign:"center"}}>30-50 <br/>VMs</td><td style={{verticalAlign:"middle"}}>&gt;&gt;</td><td>D2s<br/>VM</td>
+						</tr>
+						<tr>
+							<td colSpan={3} style={{textAlign:'center'}}>100,000 connections</td>
+						</tr>
+					</table>
+				</section>
+				<section data-background-color="#ffffff">
+					<table>
+						<tr>
+							<th style={{textAlign:"center"}}>Server<br/>size</th>
+							<th style={{textAlign:"center"}}>Total<br/>connections</th>
+						</tr>
+						<tr>
+							<td>D2s</td><td style={{textAlign:"center"}}>100k</td>
+						</tr>
+						<tr>
+							<td>D8s</td><td style={{textAlign:"center"}}>214k</td>
+						</tr>
+						<tr>
+							<td>D32s</td><td style={{textAlign:"center"}}>245k</td>
+						</tr>
+					</table>
+				</section>
+				<section>
+					<h1>Managing the pressure</h1>
 				</section>
 			</div>
         );
