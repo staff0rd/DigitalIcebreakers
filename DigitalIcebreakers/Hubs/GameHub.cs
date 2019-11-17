ef5d1b1fce53dea27214bd55ac26000b22b819c6
Stafford Williams <stafford.williams@gmail.com>
2019-11-17 17:27:59 +1100

    extract SendHelper

--- DigitalIcebreakers/Hubs/GameHub.cs
+++ DigitalIcebreakers/Hubs/GameHub.cs
@@ -13,41 +13,6 @@ using Newtonsoft.Json.Linq;
 
 namespace DigitalIcebreakers.Hubs
 {
-
-    public class SendHelper 
-    {
-        IHubCallerClients _clients;
-
-        public SendHelper(IHubCallerClients clients)
-        {
-            _clients = clients;
-        }
-
-        public IClientProxy Players(Lobby lobby) {
-            return _clients.Clients(lobby.Players.Where(p => !p.IsAdmin).Select(p => p.ConnectionId).ToList());
-        }
-
-        public IClientProxy EveryoneInLobby(Lobby lobby)
-        {
-            return _clients.Clients(lobby.Players.Select(p => p.ConnectionId).ToList());
-        }
-
-        public IClientProxy Admin(Lobby lobby)
-        {
-            return _clients.Client(lobby.Admin.ConnectionId);
-        }
-
-        public IClientProxy Self()
-        {
-            return _clients.Caller;
-        }
-
-        internal IClientProxy Player(Player player)
-        {
-            return _clients.Client(player.ConnectionId);
-        }
-    }
-
     public class GameHub : Hub, IGameHub
     {
         static int lobbyNumber = 0;
@@ -306,7 +271,10 @@ namespace DigitalIcebreakers.Hubs
         private async Task SystemMessage(string action)
         {
             var payload = JsonConvert.SerializeObject(new HubMessage { system = action });
-            await HubMessage(payload);
+            var lobby = GetLobby();
+            if (lobby != null) {
+                await HubMessage(payload);
+            }
         }
 
         public async Task HubMessage(string json) 
