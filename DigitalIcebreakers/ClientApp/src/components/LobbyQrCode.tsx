55d389798fd8dde10ca344aa290a0679a7955120
Stafford Williams <stafford.williams@gmail.com>
2020-08-27 09:42:10 +1000

    qr code height

--- DigitalIcebreakers/ClientApp/src/components/LobbyQrCode.tsx
+++ DigitalIcebreakers/ClientApp/src/components/LobbyQrCode.tsx
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStylesLg = makeStyles((theme) => ({
     container: {
-        height: '100%',
+        height: '85%',
         display: 'flex',
         flexDirection: 'column',
         textAlign: 'center',
