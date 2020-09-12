92a8050361e4f9a9a318d88904bef481da1fd950
Stafford Williams <stafford.williams@gmail.com>
2020-09-12 15:59:47 +1000

    tests

--- DigitalIcebreakers/ClientApp/src/games/Poll/components/setCorrectAnswer.test.ts
+++ DigitalIcebreakers/ClientApp/src/games/Poll/components/setCorrectAnswer.test.ts
@@ -23,7 +23,20 @@ describe('setCorrectAnswer', () => {
             answers = [ { id: '0', text: '', correct: true }, { id: '1', text: '', correct: false }];
         });
         it('should set false', () => {
-
+            const reuslt = setCorrectAnswer(answers, answers[0]);
+            expect(answers[0].correct).toBe(false);
+            expect(answers[1].correct).toBe(false);
+        });
+    });
+    describe('multple toggle from false', () => {
+        let answers: Answer[];
+        beforeEach(() => {
+            answers = [ { id: '0', text: '', correct: false }, { id: '1', text: '', correct: true }];
+        });
+        it('should set false', () => {
+            const reuslt = setCorrectAnswer(answers, answers[0]);
+            expect(answers[0].correct).toBe(true);
+            expect(answers[1].correct).toBe(false);
         });
     });
 });
