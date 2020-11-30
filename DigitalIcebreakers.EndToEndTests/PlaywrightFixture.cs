7baed48a91e44c7cd41a52db077961963d676398
Stafford Williams <stafford.williams@gmail.com>
2020-11-30 22:32:20 +1100

    env vars

--- DigitalIcebreakers.EndToEndTests/PlaywrightFixture.cs
+++ DigitalIcebreakers.EndToEndTests/PlaywrightFixture.cs
@@ -2,12 +2,25 @@ using System;
 using System.Threading.Tasks;
 using Xunit;
 using PlaywrightSharp;
+using Microsoft.Extensions.Configuration;
+using Shouldly;
 
 namespace DigitalIcebreakers.EndToEndTests
 {
     public class PlaywrightFixture : IDisposable, IAsyncLifetime
     {
-        public PlaywrightFixture() {}
+        public IPlaywright PlaywrightDriver { get; private set; }
+        public bool Headless { get; private set; }
+        public PlaywrightSharp.Chromium.IChromiumBrowser LobbyBrowser { get; private set; }
+        public IPage LobbyPage { get; private set; }
+        public string LobbyUrl { get; private set; }
+
+        public string Url { get; private set; }
+        public PlaywrightFixture(IConfiguration config)
+        {
+            Url = config.GetValue<string>("Url") ?? "http://0.0.0.0:5000";
+            Headless = config.GetValue<bool?>("Headless") ?? true;
+        }
 
         public void Dispose()
         {
@@ -30,21 +43,19 @@ namespace DigitalIcebreakers.EndToEndTests
 
         private async Task InitialiseLobby()
         {
-            LobbyBrowser = await PlaywrightDriver.Chromium.LaunchAsync();
+            LobbyBrowser = await PlaywrightDriver.Chromium.LaunchAsync(headless: Headless);
             LobbyPage = await LobbyBrowser.NewPageAsync();
-            await LobbyPage.GoToAsync("http://0.0.0.0:5000");
+            await LobbyPage.GoToAsync(Url);
             var presentButton = await LobbyPage.GetByTestId("present-button");
+            presentButton.ShouldNotBeNull();
             await presentButton.ClickAsync();
             var createButton = await LobbyPage.GetByTestId("create-lobby-button");
+            createButton.ShouldNotBeNull();
             await createButton.ClickAsync();
             var qrCode = await LobbyPage.GetByTestId("qrcode-link");
+            qrCode.ShouldNotBeNull();
             LobbyUrl = await qrCode.GetAttributeAsync("href");
         }
-
-        public IPlaywright PlaywrightDriver { get; private set; }
-        public PlaywrightSharp.Chromium.IChromiumBrowser LobbyBrowser { get; private set; }
-        public IPage LobbyPage { get; private set; }
-        public string LobbyUrl { get; private set; }
     }
 
     [CollectionDefinition("Playwright")]
