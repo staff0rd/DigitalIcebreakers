60d5b2a3b6a56d311813c23379ad331c21db526f
Stafford Williams <stafford.williams@gmail.com>
2021-01-05 02:00:51 +1100

    path fix

--- DigitalIcebreakers/ClientApp/src/store/rootReducer.ts
+++ DigitalIcebreakers/ClientApp/src/store/rootReducer.ts
@@ -4,15 +4,15 @@ import { userReducer } from "./user/reducers";
 import { RootState, GamesState } from "./RootState";
 import { lobbyReducer } from "./lobby/reducers";
 import { shellReducer } from "./shell/reducers";
-import { yesNoMaybeReducer } from "games//YesNoMaybe/YesNoMaybeReducer";
-import { doggosVsKittehsReducer } from "games//DoggosVsKittehs/DoggosVsKittehsReducer";
-import { buzzerReducer } from "games//Buzzer/BuzzerReducer";
-import { splatReducer } from "games//Splat/SplatReducer";
-import { pongReducer } from "games//Pong/PongReducer";
-import { ideaWallReducer } from "games//IdeaWall/IdeaWallReducer";
+import { yesNoMaybeReducer } from "games/YesNoMaybe/YesNoMaybeReducer";
+import { doggosVsKittehsReducer } from "games/DoggosVsKittehs/DoggosVsKittehsReducer";
+import { buzzerReducer } from "games/Buzzer/BuzzerReducer";
+import { splatReducer } from "games/Splat/SplatReducer";
+import { pongReducer } from "games/Pong/PongReducer";
+import { ideaWallReducer } from "games/IdeaWall/IdeaWallReducer";
 import { pollReducer } from "games/Poll/reducers/pollReducer";
 import { triviaReducer } from "games/Trivia/reducers/triviaReducer";
-import { namePickerReducer } from "games//NamePicker/NamePickerReducer";
+import { namePickerReducer } from "games/NamePicker/NamePickerReducer";
 import { broadcastReducer } from "games/Broadcast/BroadcastReducer";
 
 const gamesReducer = combineReducers<GamesState>({
