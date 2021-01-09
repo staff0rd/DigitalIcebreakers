1db50ed8410b7fa1c03813f1798f825217056bf7
Stafford Williams <stafford.williams@gmail.com>
2021-01-09 16:20:51 +1100

    StartStopContinue: display ideas

--- DigitalIcebreakers/ClientApp/src/games/StartStopContinue/Presenter.tsx
+++ DigitalIcebreakers/ClientApp/src/games/StartStopContinue/Presenter.tsx
@@ -10,6 +10,11 @@ import { useSelector } from "store/useSelector";
 import { RootState } from "store/RootState";
 
 const useStyles = makeStyles((theme) => ({
+  header: {},
+  waiting: {
+    marginLeft: theme.spacing(3),
+    padding: theme.spacing(2),
+  },
   card: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1),
@@ -21,30 +26,38 @@ export const Presenter = () => {
   const ideas = useSelector(
     (state: RootState) => state.games.startStopContinue.ideas
   );
+  const ideasByCategory = (category: string) =>
+    ideas.filter(
+      (idea) => idea.payload.category === Category[category as Categories]
+    );
+
   return (
-    <>
+    <ContentContainer>
       {getCategories().map((category) => (
-        <ContentContainer header={category}>
+        <>
+          <Typography variant="h4" className={classes.header}>
+            {category}
+          </Typography>
           <GridContainer>
-            {ideas
-              .filter(
-                (idea) =>
-                  idea.payload.category === Category[category as Categories]
-              )
-              .map((idea) => (
-                <GridItem xs={12} sm={12} md={4}>
-                  <Card className={classes.card}>
-                    <CardBody>
-                      <Typography variant="body1">
-                        {idea.payload.message}
-                      </Typography>
-                    </CardBody>
-                  </Card>
-                </GridItem>
-              ))}
+            {!ideasByCategory(category).length && (
+              <Typography className={classes.waiting} variant="body1">
+                Waiting for audience...
+              </Typography>
+            )}
+            {ideasByCategory(category).map((idea) => (
+              <GridItem xs={12} sm={12} md={4}>
+                <Card className={classes.card}>
+                  <CardBody>
+                    <Typography variant="body1">
+                      {idea.payload.message}
+                    </Typography>
+                  </CardBody>
+                </Card>
+              </GridItem>
+            ))}
           </GridContainer>
-        </ContentContainer>
+        </>
       ))}
-    </>
+    </ContentContainer>
   );
 };
