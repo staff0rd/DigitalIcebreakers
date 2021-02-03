86c673528bd4db026a71995aef48665c0a3a4d74
Stafford Williams <stafford.williams@gmail.com>
2021-02-03 23:08:45 +1100

    log to postgres

--- DigitalIcebreakers/Program.cs
+++ DigitalIcebreakers/Program.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Hosting;
 using Serilog;
 using Serilog.Events;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using Serilog.Sinks.PostgreSQL;
+using NpgsqlTypes;
+using Npgsql;
 
 namespace DigitalIcebreakers
 {
@@ -11,11 +15,30 @@ namespace DigitalIcebreakers
     {
         public static void Main(string[] args)
         {
+            string connectionString = "User ID=postgres;Password=example;Host=localhost;Port=5432;Database=postgres;";
+
+            string tableName = "logs";
+
+            IDictionary<string, ColumnWriterBase> columnWriters = new Dictionary<string, ColumnWriterBase>
+            {
+                { "message", new RenderedMessageColumnWriter(NpgsqlDbType.Text) },
+                { "message_template", new MessageTemplateColumnWriter(NpgsqlDbType.Text) },
+                { "level", new LevelColumnWriter(true, NpgsqlDbType.Varchar) },
+                { "raise_date", new TimestampColumnWriter(NpgsqlDbType.TimestampTz) },
+                { "exception", new ExceptionColumnWriter(NpgsqlDbType.Text) },
+                { "properties", new PropertiesColumnWriter(NpgsqlDbType.Jsonb) },
+
+            };
+
             var config = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.SpaServices", LogEventLevel.Information)
                 .Enrich.FromLogContext()
+                .WriteTo.PostgreSQL(connectionString, tableName, columnWriters, failureCallback: (e) =>
+                {
+                    Console.WriteLine("Could not log to postgres" + e.Message);
+                }, needAutoCreateTable: true, needAutoCreateSchema: true)
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}")
                 .WriteTo.Trace();
 
