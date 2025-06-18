e9de0f10d9bfffc3c48ef8ace72db5a653678786
Stafford Williams <stafford.williams@gmail.com>
2025-06-18 21:49:23 +1000

    extend timeout

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
   /* Test timeout */
-  timeout: 5000,
+  timeout: process.env.CI ? 30000 : 5000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
