e56702faec544f9ef0f936100e7d1b5607379dd5
Stafford Williams <stafford.williams@gmail.com>
2026-06-04 20:58:28 +1000

    wip poll/trivia jotai migration

--- web/src/games/Trivia/reducers/triviaReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { triviaPlayerReducer } from "./triviaPlayerReducer";
-import { triviaPresenterReducer } from "./triviaPresenterReducer";
-
-export const triviaReducer = combineReducers({
-  player: triviaPlayerReducer,
-  presenter: triviaPresenterReducer,
-});
